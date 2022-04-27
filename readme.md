c56cd5960923f5dcd7ae850b4b687a00ecd82dfd
Carter Ervin <cjrox01@yahoo.com>
2022-04-27 18:42:39 -0500

    Adjusted readme, package.json scripts, gitignore to ignore lock files, and some view structures

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
-#The Concentrated Reef
+# The Concentrated Reef
 
-##E-Commerce page for buying flowers and concentrates
+## E-Commerce page for buying flowers, concentrates, and syrups
+
+NOTE: If you are using this to view the source and want to run the server, please compile the TypeScript using `npm run build` or `yarn build` then run `npm start` or `yarn start`. Alternative if you don't care about having to build separately for production would be to use `npm run start-build` or `yarn start-build` and that will both build and start the server. For dev purposes use `npm run dev` or `yarn dev` to directly compile the TypeScript.
+
+### Routes
+| Action | Route | Description |
+| --- | --- | --- |
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,29 @@
 import express, {Request, Response} from 'express';
 import ejs from 'ejs';
 import dotenv from 'dotenv';
+import path from 'path';
+import sessions from 'express-session';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public'), {}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(sessions({
+  secret: process.env.SECRET || "notasecret123",
+  saveUninitialized: true,
+  cookie: {maxAge: Infinity},
+  resave: false
+}));
+app.use(cookieParser());
 
 app.get('*', (req: Request, res: Response) => {
-  res.render('404');
+  res.render('404', {msg: "Page not found"});
 });
 
 app.listen(PORT, ()=>{console.log("Actively Accepting requests...")});
