5b870f499fdf2606e0a25838fb0c7ad522f73130
Carter Ervin <cjrox01@yahoo.com>
2022-04-30 14:40:17 -0500

    More route things added

--- src/routes/concentrate.ts
+++ src/routes/concentrate.ts
@@ -1,5 +1,16 @@
 import {Request, Response, Router} from 'express';
+import { Concentrate } from '../models';
 
 const cct = Router();
+
+cct.get('/', async (req: Request, res: Response) => {
+  res.render('concentrate');
+});
+
+cct.get('/:name', async (req: Request, res: Response) => {
+  const name = req.params.name;
+  const cctPull = await Concentrate.findOne({name: name});
+  res.render('details/concentrate', {scheme: cctPull});
+});
 
 export default cct;
--- src/routes/syrup.ts
+++ src/routes/syrup.ts
@@ -1,5 +1,16 @@
 import {Request, Response, Router} from 'express';
+import {Syrup} from '../models';
 
 const syrup = Router();
+
+syrup.get('/', async (req: Request, res: Response) => {
+  res.render('syrup');
+});
+
+syrup.get('/:name', async (req: Request, res: Response) => {
+  const name = req.params.name;
+  const syrupPull = await Syrup.findOne({name: name});
+  res.render('details/syrup', {scheme: syrupPull});
+});
 
 export default syrup;
