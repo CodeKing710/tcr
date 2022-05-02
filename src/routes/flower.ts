9b525e21d0f3f54a781b5ddef7b8929edd2b922a
Carter Ervin <cjrox01@yahoo.com>
2022-05-02 09:57:59 -0500

    attmpted to fix tsconfig error but to no avail, still builds, renamed some files

--- src/routes/flower.ts
+++ src/routes/flower.ts
@@ -4,13 +4,21 @@ import {Flower} from '../models';
 const flower = Router();
 
 flower.get('/', async (req: Request, res: Response) => {
-  res.render('flower');
+  res.render('flowers');
 });
 
 flower.get('/:name', async (req: Request, res: Response) => {
   const name = req.params.name;
-  const flowerPull = await Flower.findOne({name: name});
-  res.render('details/flower', {scheme: flowerPull});
+  try {
+    const flowerPull = await Flower.findOne({name: name});
+    if(flowerPull !== null) {
+      res.render('details/flower', {scheme: flowerPull});
+    } else {
+      res.render('404', {title:"Flower not Found", msg: "Flower not Found!"});
+    }
+  } catch (e) {
+    res.render('404', {msg: e});
+  }
 });
 
 export default flower;
