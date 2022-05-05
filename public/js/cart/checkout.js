a2f4463dc52a00829089db9c356cd198b5563d68
Carter Ervin <cjrox01@yahoo.com>
2022-05-05 16:06:36 -0500

    Finished desktop styling and fixed some auth and checkout issues

--- public/js/cart/checkout.js
+++ public/js/cart/checkout.js
@@ -1,5 +1,10 @@
 function checkout() {
-  alert("Checkout Success");
-  window.location = window.location.origin;
+  if(sessionStorage.getItem('cart') === "") {
+    alert('Cart Empty');
+  } else {
+    alert("Checkout Success");
+  }
   sessionStorage.setItem('cart', []);
+  save();
+  window.location = window.location.origin;
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import {Request, Response, Router} from 'express';
 import bcrypt from 'bcrypt';
 import {User} from '../models';
+import { rmSync } from 'fs';
 
 const auth = Router();
 
@@ -25,24 +26,27 @@ auth.post('/', async (req: Request, res: Response) => {
       if(await bcrypt.compare(req.body?.password, user?.password ?? "")) {
         console.log("Correct Password");
         req.session.user_id = user?.username;
+        res.redirect('/');
       } else {
         console.log("Incorrect password");
         req.session.user_id = 'guest';
+        res.render('404', {id: req.session.user_id, msg: "Username or Password Incorrect!"})
       }
     } catch (e) {
       console.log(e);
+      res.render('404', {id: req.session.user_id, msg: "User not Found!"});
     }
   } else {
     //Create then login
     try {
       await User.create(req.body);
+      req.session.user_id = req.body?.username;
+      res.redirect('/');
     } catch (e) {
       console.log("Failed to create User!");
+      res.render('404', {id:req.session.user_id, msg: "Failed to create user!"});
     }
-
-    req.session.user_id = req.body?.username;
   }
-  res.redirect('/');
 });
 
 export default auth;
